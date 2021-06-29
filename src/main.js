7ec740663ebba9f2f10255a5c660c129042965bb
Evgenii <voznukzhenia@gmail.com>
2021-06-29 15:02:35 +0300

    setup scss directory

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+//import custom styles
+import '@/assets/scss/main.scss';
+
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
