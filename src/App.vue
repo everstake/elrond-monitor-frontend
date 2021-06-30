9d900f7b90d9479400664663734d39f68ac3ff76
Evgenii <voznukzhenia@gmail.com>
2021-06-30 10:47:29 +0300

    feat: basic footer

--- src/App.vue
+++ src/App.vue
@@ -3,23 +3,36 @@
     <header class="page-header">
       <AppHeader />
     </header>
+  
     <main class="page-body">
       <router-view/>
     </main>
+
+    <footer class="page-footer">
+      <AppFooter />
+    </footer>
   </div>
 </template>
 
 <script>
 import AppHeader from '@/components/header/AppHeader.vue';
+import AppFooter from '@/components/footer/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
+    AppFooter,
   }
   
 }
 </script>
 <style lang="scss">
+  #app {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 
 </style>
