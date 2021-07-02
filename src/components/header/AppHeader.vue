1678a1c879edf82c63005290add54391e22e388e
Evgenii <voznukzhenia@gmail.com>
2021-07-02 10:34:30 +0300

    refactor: header

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,73 +1,115 @@
 <template>
-  <div class="header">
-    <b-navbar type="dark" variant="dark">
-      <b-container>
-        <b-col cols="5">
-          <b-navbar-nav>
-            <b-nav-item :to="'/calculator'">APR calculator</b-nav-item>
-            <b-nav-item :to="'/guides'">Staking guides</b-nav-item>
-            <b-nav-item :to="'/web-wallet'">Web wallet</b-nav-item>
-            <b-nav-item :to="'/maiar-wallet'">Maiar wallet</b-nav-item>
-          </b-navbar-nav>
-        </b-col>
+  <div class="header white-background">
+    <div class="header__wrapper wrapper">
+      <div class="header__logo logo">
+        <img src="~@/assets/img/elrondLogo.svg" alt="logo" class="logo__img" />
+        <div class="logo__title-group">
+          <p class="logo__title">
+            Elrond monitor <br />
+            <span class="logo__subtitle"> by Everstake </span>
+          </p>
+        </div>
+      </div>
 
-        <b-col cols="3">
-          <div class="header__search-group">
-            <b-input-group>
-              <b-form-input class="mr-sm-2" placeholder="Search" />
-            </b-input-group>
-            <b-button>
-              <b-icon icon="search" />
-            </b-button>
-          </div>
-        </b-col>
+      <nav class="header__navbar navbar">
+        <ul class="navbar__list">
+          <li class="navbar__item">
+            <router-link to="/calculator" class="navbar__link">
+              APR calculator
+            </router-link>
+          </li>
+          <li class="navbar__item">
+            <router-link to="/guides" class="navbar__link">
+              Staking guides
+            </router-link>
+          </li>
+          <li class="navbar__item">
+            <router-link to="web-wallet" class="navbar__link">
+              Web wallet
+            </router-link>
+          </li>
+          <li class="navbar__item">
+            <router-link to="/maiar-wallet" class="navbar__link">
+              Maiar wallet
+            </router-link>
+          </li>
+        </ul>
+      </nav>
 
-        <b-col cols="2">
-          <div class="header__options-group">
-            <b-button class="header__option-btn">
-              <b-icon scale="1.3" icon="moon" />
-            </b-button>
-             <b-dropdown
-                variant="link"
-                id="dropdown-right"
-                no-caret
-                offset="100"
-              >
-              <template #button-content>
-                <b-button class="header__option-btn">
-                  <b-icon icon="three-dots-vertical" />
-                </b-button>
-              </template>
-              <b-dropdown-item href="#">Elrond website</b-dropdown-item>
-              <b-dropdown-item href="#">Maiar exchange</b-dropdown-item>
-              <b-dropdown-item href="#">Whitepaper</b-dropdown-item>
-              <b-dropdown-item href="#">Elrond blog</b-dropdown-item>
-              <b-dropdown-item href="#">Elrond Docs</b-dropdown-item>
-            </b-dropdown>
-            
-          </div>
-        </b-col>
-      </b-container>
-    </b-navbar>
+      <div class="header__options">
+        <button class="header__option-btn">
+          <img src="~@/assets/img/optionsIcon/moon.svg" alt="night mode">
+        </button>
+        <b-dropdown variant="link" id="dropdown-right" no-caret offset="100">
+          <template #button-content>
+            <button class="header__option-btn">
+              <img src="~@/assets/img/optionsIcon/menu.svg" alt="menu">
+            </button>
+          </template>
+          <b-dropdown-item href="#">Elrond website</b-dropdown-item>
+          <b-dropdown-item href="#">Maiar exchange</b-dropdown-item>
+          <b-dropdown-item href="#">Whitepaper</b-dropdown-item>
+          <b-dropdown-item href="#">Elrond blog</b-dropdown-item>
+          <b-dropdown-item href="#">Elrond Docs</b-dropdown-item>
+        </b-dropdown>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "AppHeader",
+  name: 'AppHeader',
 };
 </script>
 
 <style lang="scss">
-  .header {
-    &__search-group, &__options-group {
-      display: flex;
-      gap: 10px;
-      justify-content: center;
+.header {
+  display: flex;
+  justify-content: center;
+  border-bottom: 0.5px solid #e8e8e8;
+  height: 60px;
+
+  &__options {
+    display: flex;
+    align-items: center;
+    gap: 30px;
+  }
+
+  &__wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  &__option-btn {
+    background: none !important;
+    border: none !important;
+  }
+  .logo {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+
+    &__subtitle {
+      font-size: 12px;
+      color: #1f95ff;
     }
-    &__option-btn {
-      background: none !important;
-      border: none !important;
+
+    &__title {
+      line-height: 16px;
+    }
+
+    &__img {
+      height: 40px;
     }
   }
+
+  .navbar {
+    &__list {
+      display: flex;
+      align-items: center;
+      gap: 45px;
+    }
+  }
+}
 </style>
